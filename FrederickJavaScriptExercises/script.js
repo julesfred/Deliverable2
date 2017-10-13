6fb5badefd501fa4560520ed8c4936152bb74b4e
Julie Frederick <jules.tf4@yahoo.com>
2017-10-12 20:27:12 -0400

    first commit

---
+++ FrederickJavaScriptExercises/script.js
@@ -0,0 +1,76 @@
+/*EXERCISE 1
+
+var numpick=prompt("Enter a number.");
+
+var number=parseInt(numpick);
+
+var sum = 0;
+
+for (number; number>0; number--) {
+
+    var sum = sum + number;
+}
+
+alert("The sum of all the numbers up to yours is " + sum); 
+
+*/
+
+
+/* EXERCISE TWO
+var question = prompt("Do you want to play a game, yes or no?");
+var all = 0
+
+do {
+    var word = prompt("Enter a word.");
+    console.log(word);
+    var all = all + word;
+    var question = prompt("Do you want to play again, yes or no?");
+}
+while (question == 'yes');
+
+alert("all your words are: " + all)
+
+*/
+
+
+/*EXERCISE 3
+var intro = prompt("Do you want to print your name?");
+var agree = prompt("Write your name.");
+console.log(name);
+var exclaim = "!"
+
+
+while (intro == 'yes'){ 
+    var agree = agree + exclaim;
+    var hello = prompt("Hello. My name is " + agree, "press enter to move on.");
+    var intro = prompt("Would you like to print this again?")
+    }
+
+*/
+
+
+
+/* EXERCISE 4
+
+var question = prompt("What time of day is it?", "morning, noon, or evening?");
+
+
+if (question == 'morning'){
+    prompt("Since it is morning, you should be eating breakfast. We suggest eggs and toast.");
+}
+else if(question == 'noon') {
+    prompt("Since it is the afternoon, you should be eating lunch. We suggest a salad.");
+}
+
+else 
+    prompt("Since it is the evening, you should be eating dinner. We suggest chicken and rice");
+
+*/
+
+
+
+
+
+
+
+
